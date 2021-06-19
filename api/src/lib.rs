a12c744732e9e9bf1f23735e4f821527f2281f47
Blonteractor <priyanv2003@gmail.com>
2021-06-19 15:11:00 +0530

    Refactored main.rs

---
+++ api/src/lib.rs
@@ -0,0 +1,55 @@
+#[macro_use]
+extern crate serde_json;
+
+pub mod meta {
+    use dex::wrapper::error::ErrorList as DexErrorList;
+    use rocket::response::{Responder, Response};
+    use rocket::{http, request::Request, response};
+    use std::io::Cursor;
+
+    use dex::wrapper::utils::DexError;
+    use dex::wrapper::utils::DexWrappedObject;
+    pub struct ApiResponse {
+        body: String,
+        status: http::Status,
+    }
+
+    impl<'r> Responder<'r, 'static> for ApiResponse {
+        fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
+            Response::build()
+                .status(self.status)
+                .header(http::ContentType::JSON)
+                .raw_header("server", "Mangakekw using Rocket") //default is "Rocket"
+                // .raw_header("x-frame-options", "DENY") // refer to <https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options>
+                .sized_body(self.body.len(), Cursor::new(self.body))
+                .ok()
+        }
+    }
+
+    pub fn resolve<T: DexWrappedObject>(result: Result<T, DexError>) -> ApiResponse {
+        match result {
+            Ok(resp) => ApiResponse {
+                body: <T as DexWrappedObject>::serialize(&resp, false).unwrap(), // cannot return error for this
+                status: http::Status::Ok,
+            },
+            Err(e) => match e {
+                DexError::InvalidRequest(error_list) => ApiResponse {
+                    // body: json!(error_list.results).to_string(),
+                    body: <DexErrorList as DexWrappedObject>::serialize(&error_list, false)
+                        .unwrap(),
+                    status: http::Status::NotFound,
+                },
+                DexError::InvalidSchema => ApiResponse {
+                    body: json!({
+                    "error": {
+                        "short" : "Internal server error",
+                        "long": "Schema of response received by server was not recognized"
+                        }
+                    })
+                    .to_string(),
+                    status: http::Status::InternalServerError,
+                },
+            },
+        }
+    }
+}
